96714f34c74c6125d628a07ddcdde8c94a973914
sushi-chaaaan <sushi_code@outlook.jp>
2022-03-10 19:52:01 +0900

    add error handler

--- uec22/cogs/error.py
+++ uec22/cogs/error.py
@@ -15,5 +15,42 @@ class ErrorCatch(commands.Cog):
         pass
 
     @commands.Cog.listener(name="on_command_error")
-    async def _on_cmd_err(self, ctx: commands.Context, err: commands.CommandError):
-        pass
+    async def _on_cmd_err(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingRequiredArgument):
+            content = f"コマンドの実行に必要なパラメータが指定されていません。\n不足したパラメータ:{error.param.name}"
+        elif isinstance(error, commands.BadArgument):
+            content = "不適切なパラメータが指定されました。"
+        elif isinstance(error, commands.PrivateMessageOnly):
+            content = "このコマンドはDMでしか実行できません。"
+        elif isinstance(error, commands.CommandNotFound):
+            content = "指定されたコマンドは存在しません。"
+        elif isinstance(error, commands.TooManyArguments):
+            content = "指定されたパラメータが多すぎます。"
+        elif isinstance(error, commands.CommandOnCooldown):
+            content = f"このコマンドは一時的に使用できません。\n{error.retry_after}秒以上待ってからもう一度お試しください。"
+        elif isinstance(error, commands.MessageNotFound):
+            content = f"指定されたメッセージを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.MemberNotFound):
+            content = f"指定されたメンバーを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.GuildNotFound):
+            content = f"指定されたサーバーを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.UserNotFound):
+            content = f"指定されたユーザーを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.ChannelNotFound):
+            content = f"指定されたメンバーを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.ChannelNotReadable):
+            content = f"指定されたチャンネルにアクセスする権限がありません。\n```{error.argument}```"
+        elif isinstance(error, commands.ThreadNotFound):
+            content = f"指定されたスレッドを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.RoleNotFound):
+            content = f"指定されたロールを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.EmojiNotFound):
+            content = f"指定された絵文字を取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.GuildStickerNotFound):
+            content = f"指定されたステッカーを取得できませんでした。\n```{error.argument}```"
+        elif isinstance(error, commands.MissingPermissions):
+            perms = "\n".join(error.missing_permissions)
+            content = f"Botの権限が不足しています。\n```{perms}```"
+        else:
+            content = f"不明なエラーが発生しました。\n```{error}```"
+        await ctx.reply(content=content)
