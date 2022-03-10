55002e0e12a97e64f1355bdc49960c6419fdafaa
sushi-chaaaan <sushi_code@outlook.jp>
2022-03-10 19:19:05 +0900

    fix

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 utc = timezone.utc
 jst = timezone(timedelta(hours=9), "Asia/Tokyo")
 
-CORE_EXTENSION_LIST = []
+EXTENSION_LIST = []
 
 PERSISTENT_VIEWS = []
 
@@ -24,7 +24,7 @@ class MyBot(commands.Bot):
     def __init__(self, command_prefix="!ue", **options):
         super().__init__(command_prefix, **options)
         self.persistent_views_added = False
-        for cog in CORE_EXTENSION_LIST:
+        for cog in EXTENSION_LIST:
             try:
                 self.load_extension(cog)
                 print(f"Extension [{cog}.py] is loaded!")
@@ -36,14 +36,18 @@ class MyBot(commands.Bot):
             for view in PERSISTENT_VIEWS:
                 try:
                     self.add_view(view)
-                    print(f"Added View {view} !")
+                    print(f"Added View [{view}] !")
                 except Exception:
                     traceback.print_exc()
             self.persistent_views_added = True
-        info = f"Logged in as {self.user} (ID:{self.user.id})\nNow: {datetime.utcnow().astimezone(jst).strftime('%Y/%m/%d %H:%M:%S')}\nLibrary version: {discord.__version__}\nPython Info:{sys.version}"
-        print("------------------------------------------------------")
-        print(info)
-        print("------------------------------------------------------")
+        info = ""
+        if self.user is None:
+            print("cannot get self.user")
+        else:
+            info = f"Logged in as {self.user} (ID:{self.user.id})\nNow: {datetime.utcnow().astimezone(jst).strftime('%Y/%m/%d %H:%M:%S')}\nLibrary version: {discord.__version__}\nPython Info:{sys.version}"
+            print("------------------------------------------------------")
+            print(info)
+            print("------------------------------------------------------")
         channel = self.get_channel(951165574237532200)
         if isinstance(channel, discord.TextChannel):
             await channel.send(f"```{info}```")
