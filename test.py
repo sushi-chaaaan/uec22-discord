befa772343613288093f82341e26dac0a1f3aa3a
sushi-chaaaan <sushi_code@outlook.jp>
2022-03-13 17:29:02 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import sys
+from datetime import timezone, timedelta, datetime
+
+
+jst = timezone(timedelta(hours=9), "Asia/Tokyo")
 
 print(sys.version)
+print(datetime.now(jst).strftime("%Y/%m/%d %H:%M:%S"))
+
+a = ""
+if a is not None:
+    print("Russia")
