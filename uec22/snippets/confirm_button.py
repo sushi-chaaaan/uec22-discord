389008d5c0c4e3e9bdb0ffa0293cb5d9a869c03a
sushi-chaaaan <sushi_code@outlook.jp>
2022-03-28 04:39:18 +0900

    initial commit

--- uec22/snippets/confirm_button.py
+++ uec22/snippets/confirm_button.py
@@ -1,10 +1,40 @@
 import asyncio
-import discord
 from typing import Optional
 
+import discord
 
-# 基本的に await asyncio.Future()な形で運用する前提です。
-# いい感じにViewに Accept_ButtonとReject_Buttonをくっつけてあげてください
+from snippets.disable_view import Disabled_View
+
+# 基本的に await asyncio.Future()な形で運用する前提です。ボタンはbool(承認/非承認結果)とinteractionオブジェクトのタプルを返します。
+# 基本的にConfirmViewをそのままつかってください
+
+
+class ConfirmView(discord.ui.View):
+    def __init__(
+        self,
+        label_ok: str,
+        label_ng: str,
+        future: Optional[asyncio.Future] = None,
+        disabled: bool = False,
+        timeout: Optional[float] = None,
+    ):
+        d_view = DisabledConfirmView(ok=label_ok, ng=label_ng)
+        super().__init__(timeout=timeout)
+        self.add_item(
+            Accept_Button(
+                label=label_ok, future=future, disabled=disabled, disabled_view=d_view
+            )
+        )
+        self.add_item(
+            Reject_Button(
+                label=label_ng, future=future, disabled=disabled, disabled_view=d_view
+            )
+        )
+
+
+class DisabledConfirmView(ConfirmView):
+    def __init__(self, ok: str, ng: str):
+        super().__init__(label_ok=ok, label_ng=ng, disabled=True)
 
 
 class Accept_Button(discord.ui.Button):
@@ -16,18 +46,21 @@ class Accept_Button(discord.ui.Button):
         disabled: bool = False,
         custom_id: Optional[str] = None,
         row: Optional[int] = None,
-        future: asyncio.Future,
+        future: Optional[asyncio.Future] = None,
+        disabled_view: discord.ui.View,
     ):
         self.future = future
+        self.d_view = disabled_view
         super().__init__(
             style=style, label=label, disabled=disabled, custom_id=custom_id, row=row
         )
 
     async def callback(self, interaction: discord.Interaction):
-        self.future.set_result([True, interaction])
+        if self.future:
+            self.future.set_result([True, interaction])
         await interaction.response.defer()
-        if interaction.message is not None:
-            await interaction.message.delete()
+        if interaction.message:
+            await interaction.message.edit(view=self.d_view)
 
 
 class Reject_Button(discord.ui.Button):
@@ -39,18 +72,21 @@ class Reject_Button(discord.ui.Button):
         disabled: bool = False,
         custom_id: Optional[str] = None,
         row: Optional[int] = None,
-        future: asyncio.Future,
+        future: Optional[asyncio.Future] = None,
+        disabled_view: discord.ui.View,
     ):
         self.future = future
+        self.d_view = disabled_view
         super().__init__(
             style=style, label=label, disabled=disabled, custom_id=custom_id, row=row
         )
 
     async def callback(self, interaction: discord.Interaction):
-        self.future.set_result([False, interaction])
+        if self.future:
+            self.future.set_result([False, interaction])
         await interaction.response.defer()
-        if interaction.message is not None:
-            await interaction.message.delete()
+        if interaction.message:
+            await interaction.message.edit(view=self.d_view)
 
 
 class Confirm_Button(discord.ui.Button):
