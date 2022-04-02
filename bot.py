37c8a85e27fe221b8c27373a61b0e10a8e9d59fb
sushi-chaaaan <sushi_code@outlook.jp>
2022-04-02 12:53:09 +0900

    fix

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ import discord
 from discord.ext import commands
 
 from data.firestore import get_data
+from uec22.cogs.entrance import EnterVerifyView
 from uec22.cogs.forum import QuestionView
 from uec22.role_panel.panel_db import set_board
 
@@ -57,7 +58,10 @@ class MyBot(commands.Bot):
 
     # run when boot is done preparing to run.
     async def on_ready(self):
-        PERSISTENT_VIEWS = [QuestionView()]
+        PERSISTENT_VIEWS = [
+            QuestionView(),
+            EnterVerifyView(),
+        ]
         if not self.persistent_views_added:
             for view in PERSISTENT_VIEWS:
                 try:
