f036b4c7b9dcb7681ae3a639f17daaaaea852e6e
sushi-chaaaan <sushi_code@outlook.jp>
2022-04-04 20:43:08 +0900

    fix

--- uec22/cogs/utils.py
+++ uec22/cogs/utils.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta, timezone
 
 import discord
 from discord import ApplicationContext, Option
-from discord.commands import permissions, slash_command
+from discord.commands import slash_command
 from discord.ext import commands
-from ids import admin_role, guild_id
+from ids import guild_id
 
 utc = timezone.utc
 jst = timezone(timedelta(hours=9), "Asia/Tokyo")
