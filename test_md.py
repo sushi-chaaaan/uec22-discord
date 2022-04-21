55f585f49f56e04158970753aba52befa6f7049d
sushi-chaaaan <sushi_code@outlook.jp>
2022-04-22 01:21:53 +0900

    :think:

--- test_md.py
+++ test_md.py
@@ -1,6 +1,29 @@
 import subprocess
 
-subprocess.run(
-    "pandoc -t pdf -s -o tmp/report_2.pdf --pdf-engine=wkhtmltopdf -c styles/github-markdown.css tmp/report_2.md",
-    shell=True,
-)
+
+def md2html(filename: str):
+    pure_name = filename.removesuffix(".md")
+    subprocess.run(
+        f"pandoc -s -o {pure_name}.html --mathjax -c styles/github-markdown.css {filename}",
+        shell=True,
+    )
+    return f"{pure_name}.html"
+
+
+def html2pdf(filename: str):
+    pure_name = filename.removesuffix(".html")
+    subprocess.run(
+        f"wkhtmltopdf --disable-smart-shrinking --margin-top 20 --margin-left 20 --margin-right 20 --margin-bottom 20 --footer-center '[page]/[topage]' {filename} {pure_name}.pdf",
+        shell=True,
+    )
+    return f"{pure_name}.pdf"
+
+
+def main():
+    html = md2html("tmp/report_2.md")
+    pdf = html2pdf(html)
+    print(pdf)
+
+
+if __name__ == "__main__":
+    main()
